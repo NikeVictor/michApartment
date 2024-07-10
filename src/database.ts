34e27ed8145a83119f5d66b3bc29d7ae781eade3
NikeVictor <nikevictorchijindu@gmail.com>
2024-07-10 16:55:15 +0100

    fix

--- src/database.ts
+++ src/database.ts
@@ -2,8 +2,10 @@ require("dotenv").config();
 import { Sequelize, DataTypes } from "sequelize";
 import { init } from "./models/users.model";
 
-const POSTGRES_URL = process.env.DATABASE_URL as string;
-
+const POSTGRES_URL = process.env.DATABASE_URL;
+if (!POSTGRES_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
 const sequelize = new Sequelize(POSTGRES_URL, {
   dialectOptions: {
     dialect: 'postgres'
