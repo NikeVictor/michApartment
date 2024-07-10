4d049d36b606bc65426564542afc893dfaabad96
NikeVictor <nikevictorchijindu@gmail.com>
2024-07-10 16:42:21 +0100

    fix

--- src/database.ts
+++ src/database.ts
@@ -4,13 +4,16 @@ import { init } from "./models/users.model";
 
 const POSTGRES_URL = process.env.DATABASE_URL as string;
 
-const sequelize = new Sequelize(POSTGRES_URL, {dialect: 'postgres'});
-
+const sequelize = new Sequelize(POSTGRES_URL, {
+  dialectOptions: {
+    dialect: 'postgres'
+  }
+});
 init.connectModelAttrs(sequelize);
 async function connectDB() {
   try {
     await sequelize.authenticate();
-    console.log("✅ Connection has been established successfully.");
+    //console.log("✅ Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
