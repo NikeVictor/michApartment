5f38132808c11cdb50be2793f3995cf01feb9b80
NikeVictor <nikevictorchijindu@gmail.com>
2024-08-03 06:23:09 +0100

    update

--- config/config.js
+++ config/config.js
@@ -1,11 +1,24 @@
-require('dotenv').config();
-
+require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USER || 'your_username',
-    password: process.env.POSTGRES_PASSWORD || 'your_password',
-    database: process.env.DB_NAME || 'michApartment',
-    host: process.env.POSTGRES_HOST || '127.0.0.1',
-    dialect: 'postgres'
-  }
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
 };
